3f902b897b658ae01e8198977e50599ddad49974
Eric Juta <ericjohn.juta@consensys.net>
2018-07-10 13:03:28 +0100

    Update Dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,24 @@
 FROM gcr.io/kauri-197812/kauri-contract-abis:latest-dev
 
-# RUN apt-get update && apt upgrade && \
-#   apt install -y bash git openssh-client
+# setup workspace
 RUN mkdir -p /usr/src/app/packages/kauri-web
 RUN mkdir -p /usr/src/app/packages/kauri-components
 WORKDIR /usr/src/app/packages/kauri-web
+
 RUN npm install -g --unsafe-perm esy@preview
 
-# Build common components with bucklescript toolset
+# kauri-components, ReasonML
 COPY packages/kauri-components/package.json /usr/src/app/packages/kauri-components/
 COPY packages/kauri-components/esy.lock /usr/src/app/packages/kauri-components/
-COPY packages/kauri-components /usr/src/app/packages/kauri-components/
 RUN cd /usr/src/app/packages/kauri-components && esy install
+RUN apt-get update && apt-get install -y rsync
 RUN cd /usr/src/app/packages/kauri-components && esy build
 RUN cd /usr/src/app/packages/kauri-components && npm run link-ppx
 
+COPY packages/kauri-components /usr/src/app/packages/kauri-components/
+RUN cd /usr/src/app/packages/kauri-components && npm run build
+
+# kauri-web, ReasonML
 COPY packages/kauri-web/package.json /usr/src/app/packages/kauri-web/
 COPY packages/kauri-web/esy.lock /usr/src/app/packages/kauri-web/
 RUN esy install
@@ -22,9 +26,10 @@ RUN esy install
 COPY packages/kauri-web /usr/src/app/packages/kauri-web/
 RUN npm run build
 
+# env settings
 ENV GETH_BLOCKCHAIN=35.231.60.112:8545
 ENV MONOLITH_EXTERNAL_API=api.dev.kauri.io
 ENV MONOLITH_API=monolith.dev:8080
-#next specific build
 EXPOSE 3000
+
 CMD "npm" "run" "start"
--- packages/kauri-web/pages/_document.js
+++ packages/kauri-web/pages/_document.js
@@ -20,21 +20,21 @@ if (isProduction) {
   // HotJar
   scripts.push({
     crossOrigin: 'anonymous',
-    innerHTML: config.hotJarTrackingCode
+    innerHTML: config.hotJarTrackingCode,
   })
 
   // Google Tag Manager
   scripts.push({
     async: true,
-    src: 'https://www.googletagmanager.com/gtag/js?id=UA-112179323-1'
+    src: 'https://www.googletagmanager.com/gtag/js?id=UA-112179323-1',
   })
   scripts.push({
-    innerHTML: config.googleTagManagerCode
+    innerHTML: config.googleTagManagerCode,
   })
 }
 
 export default class MyDocument extends Document {
-  static async getInitialProps({ renderPage }) {
+  static async getInitialProps ({ renderPage }) {
     const sheet = new ServerStyleSheet()
     const page = renderPage(App => props => <App {...props} />)
     const styleTags = sheet.getStyleElement()
@@ -43,11 +43,11 @@ export default class MyDocument extends Document {
       ...page,
       styleTags,
       ...styles,
-      helmet: Helmet.renderStatic()
+      helmet: Helmet.renderStatic(),
     }
   }
 
-  constructor(props) {
+  constructor (props) {
     super(props)
     const { __NEXT_DATA__, ids } = this.props
     if (typeof window !== 'undefined') {
@@ -58,27 +58,27 @@ export default class MyDocument extends Document {
   }
 
   // should render on <html>
-  get helmetHtmlAttrComponents() {
+  get helmetHtmlAttrComponents () {
     return this.props.helmet.htmlAttributes.toComponent()
   }
 
   // should render on <body>
-  get helmetBodyAttrComponents() {
+  get helmetBodyAttrComponents () {
     return this.props.helmet.bodyAttributes.toComponent()
   }
 
   // should render on <head>
-  get helmetHeadComponents() {
+  get helmetHeadComponents () {
     return Object.keys(this.props.helmet)
       .filter(el => el !== 'htmlAttributes' && el !== 'bodyAttributes')
       .map(el => this.props.helmet[el].toComponent())
   }
 
-  get helmetJsx() {
+  get helmetJsx () {
     return (
       <Helmet
         htmlAttributes={{ lang: 'en' }}
-        title="Kauri"
+        title='Kauri'
         meta={[{ name: 'viewport', content: 'width=1280' }, { charSet: 'utf8' }]}
         // link={[]}
         script={scripts}
@@ -90,16 +90,16 @@ export default class MyDocument extends Document {
     )
   }
 
-  render() {
+  render () {
     return (
       <html {...this.helmetHtmlAttrComponents}>
         <Head>
           {this.helmetJsx}
           {this.helmetHeadComponents}
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://transloadit.edgly.net/releases/uppy/v0.24.3/dist/uppy.min.css" />
-          <link rel="stylesheet" type="text/css" href="/static/styles.css" />
-          <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js" />
+          <link rel='icon' href='/favicon.ico' />
+          <link rel='stylesheet' href='https://transloadit.edgly.net/releases/uppy/v0.24.3/dist/uppy.min.css' />
+          <link rel='stylesheet' type='text/css' href='/static/styles.css' />
+          <script defer src='https://use.fontawesome.com/releases/v5.0.6/js/all.js' />
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
           {this.props.styleTags}
         </Head>

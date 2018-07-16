ff2198c7a538062b3f1019190cd16718c57a82e7
Eric Juta <ericjohn.juta@consensys.net>
2018-07-16 14:49:05 +0100

    Generate approve article hash

--- packages/kauri-web/lib/generate-approve-article-hash.js
+++ packages/kauri-web/lib/generate-approve-article-hash.js
@@ -1,10 +1,45 @@
-const web3 = window.web3
+import bs58 from 'bs58'
 
-utils.keccak256(
-  web3.padRight(web3.fromAscii(id), 66),
-  resultArt.version,
-  web3.padRight(web3.fromAscii(request_id), 66),
-  web3.padRight(utils.convertIpfsHash(content_hash), 66),
-  web3.padRight(web3.fromAscii(category), 66),
-  '0x' + contributor
-)
+const generateApproveArticleHash = (id, version, content_hash, category, request_id, contributor) => {
+  const web3 = window.web3
+  const keccak256 = function (...args) {
+    args = args.map(arg => {
+      if (typeof arg === 'string') {
+        if (arg.substring(0, 2) === '0x') {
+          return arg.slice(2)
+        } else {
+          return web3.toHex(arg).slice(2)
+        }
+      }
+
+      if (typeof arg === 'number') {
+        return web3.padLeft(arg.toString(16), 64, 0)
+      } else {
+        return ''
+      }
+    })
+
+    args = args.join('')
+    console.log(args)
+    const result = web3.sha3(args, { encoding: 'hex' })
+    return result
+  }
+
+  const convertIpfsHash = ipfsHash => {
+    const decoded = bs58.decode(ipfsHash)
+
+    const result = `0x${decoded.slice(2).toString('hex')}`
+    return result
+  }
+
+  return keccak256(
+    web3.padRight(web3.fromAscii(id), 66),
+    version,
+    web3.padRight(web3.fromAscii(request_id), 66),
+    web3.padRight(convertIpfsHash(content_hash), 66),
+    web3.padRight(web3.fromAscii(category), 66),
+    contributor
+  )
+}
+
+export default generateApproveArticleHash

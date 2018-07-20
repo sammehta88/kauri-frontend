c84d78c576a6ca69140d19271aaac69e78821372
Eric Juta <ericjohn.juta@consensys.net>
2018-07-20 15:59:55 +0100

    Add TopicModerator contract to smartContracts window object

--- packages/kauri-web/lib/init-smart-contracts.js
+++ packages/kauri-web/lib/init-smart-contracts.js
@@ -3,16 +3,22 @@ import contract from 'truffle-contract'
 
 const KauriCoreArtifact = require(process.env.KauriCoreArtifact)
 const WalletArtifact = require(process.env.WalletArtifact)
+const TopicModeratorArtifact = require(process.env.TopicModeratorArtifact)
 
 let smartContracts
 
 export const initSmartContracts = web3 => {
   let KauriCore = contract(KauriCoreArtifact)
   let Wallet = contract(WalletArtifact)
+  let TopicModerator = contract(TopicModeratorArtifact)
   if (typeof web3 !== 'undefined') {
     KauriCore.setProvider(web3.currentProvider)
     Wallet.setProvider(web3.currentProvider)
-    const smartContractsToDeploy = { KauriCore: KauriCore.deployed(), Wallet: Wallet.deployed() }
+    const smartContractsToDeploy = {
+      KauriCore: KauriCore.deployed(),
+      Wallet: Wallet.deployed(),
+      TopicModerator: TopicModerator.deployed(),
+    }
 
     return Observable.concat(...Object.values(smartContractsToDeploy))
       .reduce((current, next, index) => {

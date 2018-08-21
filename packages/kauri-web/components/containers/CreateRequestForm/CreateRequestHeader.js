67abb30a8f40c6659dc33083d8d7678b6fad19f4
Eric Juta <ericjohn.juta@consensys.net>
2018-08-21 14:07:30 +0100

    Some media queries for approved article header

--- packages/kauri-web/components/containers/CreateRequestForm/CreateRequestHeader.js
+++ packages/kauri-web/components/containers/CreateRequestForm/CreateRequestHeader.js
@@ -36,6 +36,9 @@ export const CreateRequestSecondaryHeader = styled.div`
   padding: ${props => props.theme.paddingTop} ${props => props.theme.padding};
   background-color: ${props => props.theme.primaryTextColor};
   ${props => props.type === 'article' && articleHeaderCss};
+  @media(max-width: 500px) {
+    padding: ${props => props.theme.paddingTop} 10px;
+  }
 `
 
 const Logo = styled.img`

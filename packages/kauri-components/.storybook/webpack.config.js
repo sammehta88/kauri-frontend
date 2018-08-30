abd36f8c86bf3d1b8ced04813533d49cbef61d94
Eric Juta <ericjohn.juta@consensys.net>
2018-08-30 17:33:23 +0100

    Shared babel config

--- packages/kauri-components/.storybook/webpack.config.js
+++ packages/kauri-components/.storybook/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const babelConfig = require('../../kauri-web/babel.config')
 
 // Export a function. Accept the base config as the only param.
 module.exports = (storybookBaseConfig, configType) => {
@@ -10,6 +11,10 @@ module.exports = (storybookBaseConfig, configType) => {
   // Make whatever fine-grained changes you need
   // console.log(storybookBaseConfig)
   storybookBaseConfig.plugins.push(new webpack.EnvironmentPlugin(['STORYBOOK']))
+
+  storybookBaseConfig.module.rules[0].use[0].loader = require.resolve('babel-loader')
+  storybookBaseConfig.module.rules[0].use[0].options.presets = babelConfig.presets
+  storybookBaseConfig.module.rules[0].use[0].options.plugins = babelConfig.plugins
 
   // Return the altered config
   return storybookBaseConfig
--- packages/kauri-web/babel.config.js
+++ packages/kauri-web/babel.config.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   presets: [
     '@babel/preset-stage-0',
     [

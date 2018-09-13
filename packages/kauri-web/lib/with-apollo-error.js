b29942a9ddf373f055514b1b003894cd7fe38228
Eric Juta <ericjohn.juta@consensys.net>
2018-09-13 10:55:27 +0100

    Default to bad ux

--- packages/kauri-web/lib/with-apollo-error.js
+++ packages/kauri-web/lib/with-apollo-error.js
@@ -7,9 +7,12 @@ const Message = styled.h4`
   text-align: center;
   width: 100%;
   color: red;
-`;
+`
 
-const ErrorMessage = ({ data: { error: { message } } }) => <Message>{message}</Message>
+const ErrorMessage = ({ data: { error: { message } } }) =>
+  process.env.NODE_ENV === 'production'
+    ? <Message>Something went wrong.</Message>
+    : <Message>{message}</Message>
 
 export default (component = ErrorMessage, propName = 'data') =>
   branch(props => props[propName] && props[propName].error, renderComponent(component))

0fb6fcf1d8f76e8d4929b66a6e3d93c64f96eb77
Eric Juta <ericjohn.juta@consensys.net>
2018-09-18 11:39:03 +0100

    Add icon to iconnoborderbuttonthingymajig

--- packages/kauri-components/components/Button/IconNoBorderButton.js
+++ packages/kauri-components/components/Button/IconNoBorderButton.js
@@ -1,9 +1,11 @@
 // @flow
 import React from 'react'
 import styled from 'styled-components'
-import { fontSize, fontWeight, color } from 'styled-system'
+import { fontSize, fontWeight, color, space } from 'styled-system'
 
 const IconNoBorderButton = styled.button`
+  display: flex;
+  align-items: center;
   border: none;
   background-color: transparent;
   cursor: pointer;
@@ -12,6 +14,11 @@ const IconNoBorderButton = styled.button`
   ${fontSize};
   ${fontWeight};
   ${color};
+  > :first-child {
+    height: 18px;
+    width: 18px;
+    ${space};
+  }
 `
 
 type Props = {
@@ -19,8 +26,8 @@ type Props = {
   children: React.ChildrenArray<T>,
 }
 
-export default ({ icon, children, fontWeight = 700, fontSize = 0, color = 'white' }: Props) =>
-  <IconNoBorderButton color={color} fontSize={fontSize} fontWeight={fontWeight}>
+export default ({ icon, children, fontWeight = 700, fontSize = 0, space = 2, color = 'white' }: Props) =>
+  <IconNoBorderButton mr={space} color={color} fontSize={fontSize} fontWeight={fontWeight}>
     {icon}
     {children}
   </IconNoBorderButton>
--- packages/kauri-components/js-stories/Button.stories.js
+++ packages/kauri-components/js-stories/Button.stories.js
@@ -3,7 +3,9 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import IconNoBorderButton from '../components/Button/IconNoBorderButton'
 
+const UploadIcon = () => <img src='https://png.icons8.com/color/50/000000/upload.png' />
+
 storiesOf('Button', module)
   .add('IconNoBorderButton', () => (
-    <IconNoBorderButton>Background Image</IconNoBorderButton>
+    <IconNoBorderButton icon={<UploadIcon />}>Background Image</IconNoBorderButton>
   ))
